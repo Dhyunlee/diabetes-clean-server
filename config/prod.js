2acebc7f51e657cf4a372e80e9ead85852976ba4
Dhyunlee <skyblue5030@naver.com>
2022-10-16 15:32:25 +0900

    Setting: config 설정 및 간단히 웹서버 생성

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+const mongoURI = process.env.MONGO_URI;
+const port = process.env.PORT;
+
+export default {
+  mongoURI,
+  port,
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-/** Empty --> */ 
+import express from "express";
+import path from "path";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import config from '../config/key.js';
+
+const app = express();
+const {mongoURI, port} = config;
+
+
+app.get('/', (req, res) => res.json({isSucess: true}))
+
+app.listen(port, () => console.log(`Server Listening on ${port}`));
