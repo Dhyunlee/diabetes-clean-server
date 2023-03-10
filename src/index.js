455296bf0aa49c5ee98a5cec52ff68fe65ec1f24
Dhyunlee <skyblue5030@naver.com>
2023-03-10 11:01:34 +0900

    Feat(index): core 설정

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import express from "express";
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import config from "./config/key.js";
 import dbconnect from "./dbconfig.js";
 import usersRouter from "./routes/users.js";
-import authRouter from './routes/auth.js';
-import diabetesRouter from './routes/diabetes.js';
+import authRouter from "./routes/auth.js";
+import diabetesRouter from "./routes/diabetes.js";
 
 const app = express();
 const { PORT, COOKIE_SECRET } = config;
 
-dbconnect()
-app.use(cors());
+dbconnect();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser(COOKIE_SECRET));
@@ -22,6 +27,4 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/diabetes", diabetesRouter);
 app.use("/", (req, res) => res.json({ isSucess: true }));
 
-app.listen(PORT, () =>
-  console.log(`Server Listening on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server Listening on http://localhost:${PORT}`));
