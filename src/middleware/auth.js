0f528d7a5142d4b4189c9eec2921691d4f511356
Dhyunlee <skyblue5030@naver.com>
2023-03-19 02:11:43 +0900

    Fix(middleware): req.headers를 통한 인증 검증으로 수정

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,23 +2,29 @@ import day from "dayjs";
 import User from "../models/user.js";
 
 let auth = async (req, res, next) => {
-  let token = req.cookies.token;
-  if (!token) return next(); // 토큰 없음
+  let token =
+  req.headers.hasOwnProperty("authorization") && req.headers.authorization.split("Bearer ")[1];
+  if (token === 'undefined') {
+    // 토큰 없음
+    return res.status(401).json({ isOk: false, msg: "인증이 필요합니다." });
+  } 
 
   // 토큰 검증
   User.verifyToken(token, async (err, decoded) => {
-    if (err) throw err;
-    const user = await User.findById(decoded._id);
-    const now = Math.floor(day().valueOf() / 1000);
-    if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
-      console.log("토큰 발급");
-      const token = user.generateToken();
-      res.cookie("access_token", token, {
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-        httpOnly: true,
-      });
+    if (err) {
+      return res.status(403).json({ isOk: false, msg: "토큰이 유효하지 않습니다." });
     }
-    const {password, ...userData} = user._doc;
+    const user = await User.findById(decoded._id);
+    // const now = Math.floor(day().valueOf() / 1000);
+    // if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
+    //   console.log("토큰 발급");
+    //   const token = user.generateToken();
+    //   res.cookie("access_token", token, {
+    //     maxAge: 1000 * 60 * 60 * 24 * 7,
+    //     httpOnly: true,
+    //   });
+    // }
+    const { password, ...userData } = user._doc;
     req.user = userData;
     next();
     /* 
