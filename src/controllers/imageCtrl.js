3be739c2e45a5fdc31107b8589661e4bfb50ec69
Dhyunlee <skyblue5030@naver.com>
2023-04-30 00:51:00 +0900

    Feat(controllers): 댓글 좋아요, 컨텐츠 공감, 이미지 업로드 컨트롤러 추가

--- src/controllers/imageCtrl.js
+++ src/controllers/imageCtrl.js
@@ -1,11 +1,13 @@
-import Image from '../models/image';
+import Image from "../models/image";
 
 export const imageCtrl = {
-   postUpload: async (req, res) => {
-     try {
-       const imgae = new Image();
-     } catch(err) {
-        return res.status(500).json(err)
-     }
-   }
-}
+  postUploadImg: async (req, res) => {
+    const image = new Image(req.body);
+    try {
+      const savedImageUrl = await image.save();
+      res.status(200).json(savedImageUrl);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  },
+};
