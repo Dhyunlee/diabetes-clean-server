c879712195cb0775b9a91f41e3545fe80cfed8c1
Dhyunlee <skyblue5030@naver.com>
2023-05-21 20:46:17 +0900

    Feat: api path 상수로 관리

---
+++ src/constants/path.js
@@ -0,0 +1,8 @@
+export const INDEX_PATH = '/';
+export const FIND_BY_ID = '/:id'
+export const GET_USER_FIND_BY_ID = '/users/:id'
+export const LOG_IN = '/login'; 
+export const LOG_OUT = '/logout'; 
+export const CHECK_EMAIL = '/checkemail'; 
+export const GET_ALL_COMMENT = '/contents/:contentsId'
+export const GET_USER_COMMENT = '/user/:userId'
