79009c1bd23d54e8aef35028fdb66d75350eb2e7
Dhyunlee <skyblue5030@naver.com>
2023-06-20 14:31:34 +0900

    Chore: follow 모델 삭제 follow 상태는 user 모델에서 관리하기로함

--- src/constants/path.js
+++ src/constants/path.js
@@ -1,7 +1,8 @@
-export const INDEX_PATH = '/';
-export const FIND_BY_ID = '/:id'
-export const GET_USER_FIND_BY_ID = '/users/:userId'
-export const LOG_IN = '/login'; 
-export const LOG_OUT = '/logout'; 
-export const CHECK_EMAIL = '/checkemail'; 
-export const GET_FIND_BY_CONTENTS_ID = '/contents/:contentsId'
+export const INDEX_PATH = "/";
+export const FIND_BY_ID = "/:id";
+export const GET_USER_FIND_BY_ID = "/users/:userId";
+export const GET_USER_CONTENTS = "/users/:nickname";
+export const LOG_IN = "/login";
+export const LOG_OUT = "/logout";
+export const CHECK_EMAIL = "/checkemail";
+export const GET_FIND_BY_CONTENTS_ID = "/contents/:contentsId";
