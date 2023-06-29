38cd509f815ed0da9a12912dfaa2705b14f82549
Dhyunlee <skyblue5030@naver.com>
2023-06-29 12:56:00 +0900

    Feat: empathyCtrl routes 구현

--- src/routes/empathy.js
+++ src/routes/empathy.js
@@ -1,1 +1,17 @@
-// 공감 기능
+import express from "express";
+import { empathyCtrl } from "../controllers/empathyCtrl";
+const router = express.Router();
+
+// @routes     POST api/v1/empathy/contents/:id
+// @desc       empathy 추가
+router.post("/", auth, empathyCtrl.empathy);
+
+// @routes     POST api/v1/empathy/contents/:id
+// @desc       empathy 취소
+router.delete("/", auth, empathyCtrl.unEmpathy);
+
+// @routes     POST api/v1/empathy/contents/:id
+// @desc       empathy 데이터 조회
+router.get("/", auth, empathyCtrl.getEmpathy);
+
+export default router;
