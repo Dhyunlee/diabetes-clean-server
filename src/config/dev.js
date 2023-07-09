f60dc84058a0e3b37e52a2278c1075b45dc5d314
Dhyunlee <skyblue5030@naver.com>
2023-07-10 00:18:32 +0900

    Chore: eslint 설정

---
+++ src/config/dev.js
@@ -0,0 +1,18 @@
+const MONGO_URI =
+  "mongodb+srv://skyblue5030:dong5372@producer.pfpqnqh.mongodb.net/dclean?retryWrites=true&w=majority";
+const PORT = 5000;
+const JWT_CONFIG = {
+  secret:
+    "3720e4764a4d160cec86a91d022c7b542569c3212b6921386feea79cce14582499b5f782488edaecd797f2460b98be3a1b3d4b2ac8bac747ea215076b16415e1",
+  algorithm: "HS256",
+  shortExpiresIn: "1d",
+  // longExpiresIn: "7d",
+};
+const COOKIE_SECRET = "769af81b27662ee90255ff";
+
+export default {
+  MONGO_URI,
+  PORT,
+  COOKIE_SECRET,
+  JWT_CONFIG
+};
