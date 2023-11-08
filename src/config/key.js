3bfaa1afd2b7d28a4d34421eb02e3214c691e679
hyun0509 <skyblue5030@gmail.com>
2023-11-08 22:32:07 +0900

    환경 변수 변경

--- src/config/key.js
+++ src/config/key.js
@@ -2,3 +2,4 @@ import prodConfig from "./prod.js";
 import devConfig from "./dev.js";
 
 export default process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+console.log({ nodeEnv: process.env.NODE_ENV });
